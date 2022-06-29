3934c2edf0f7983251405e046d612a87be721e3e
jmoletteire <jmoletteire@ufl.edu>
2022-06-28 20:25:45 -0400

    test commit

---
+++ PyPoll.py
@@ -0,0 +1,10 @@
+# The data we need to retrieve:
+# 1. The total number of votes cast
+# 2. A complete list of candidate who received votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. The winner of the election based on popular vote
+
+import datetime as dt
+now = dt.datetime.now()
+print(f"The time right now is {now}")
